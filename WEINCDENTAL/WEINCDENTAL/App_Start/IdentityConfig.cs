411f8441a348eb06df73e09aff8b8fd6a3f369fd
VENOM <weincreative@gmail.com>
2019-03-19 13:49:30 +0300

    temiz bir başlangıç herşey yüklü blank page hazır

---
+++ WEINCDENTAL/WEINCDENTAL/App_Start/IdentityConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace WEINCDENTAL
+{
+    public static class IdentityConfig
+    {
+        public static void RegisterIdentities()
+        {
+            // Ensures the default demo user is available to login with
+            //UserManager.Seed();
+        }
+    }
+}
