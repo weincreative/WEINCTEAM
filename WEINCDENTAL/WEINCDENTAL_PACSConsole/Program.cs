7ab050d2f07d2885860ae81368a084cb38314edb
VENOM <weincreative@gmail.com>
2019-07-17 02:22:06 +0300

    diğer pacs programindan gelenleride alıyor run admin de calisiyor

--- WEINCDENTAL/WEINCDENTAL_PACSConsole/Program.cs
+++ WEINCDENTAL/WEINCDENTAL_PACSConsole/Program.cs
@@ -8,17 +8,23 @@ using System.Threading;
 
 namespace WEINCDENTAL_PACSConsole
 {
+
     class Program
     {
 
+        #region Pacs JPG Alıcı
         static void Goruntuler()
         {
-
             Console.WriteLine("Pacs Alıcı Kontrolü Başladı.! ");
             Console.WriteLine(Environment.NewLine);
         PacsTimer:
             try
             {
+                if (!System.IO.Directory.Exists("D:project/PacsMemory") && !System.IO.Directory.Exists("D:project/Pacs"))
+                {
+                    KlasorControl();
+                }
+
                 string[] goruntuAl = System.IO.Directory.GetFiles("D:project/PacsMemory");
                 if (goruntuAl.Count() != 0)
                 {
@@ -26,12 +32,13 @@ namespace WEINCDENTAL_PACSConsole
                     Console.WriteLine($"PACS :  {nnow}");
                     foreach (var item in goruntuAl)
                     {
+                        string itemName = Path.GetFileName(item);
+                        string subs = itemName.Substring(0, itemName.Length - 4);
+
+                        #region iDixel Prepikal PACS
                         if (item.Contains(".txt"))
                         {
-                            string itemName = Path.GetFileName(item);
-                            string subs = itemName.Substring(0, itemName.Length - 4);
                             System.IO.FileInfo PacsBoyutu = new System.IO.FileInfo(item);
-                            Console.Write(Environment.NewLine + $"{subs} _{PacsBoyutu.Length} : ");
                             FileStream fs = new FileStream(item, FileMode.Open, FileAccess.Read);
                             StreamReader sw = new StreamReader(fs, Encoding.GetEncoding("iso-8859-9"), false);
                             string tc = null;
@@ -64,17 +71,21 @@ namespace WEINCDENTAL_PACSConsole
                             string orjFile = $"D:\\project\\PacsMemory\\{subs}.jpg";
                             string copyFile = $"D:\\project\\Pacs\\{tc}\\{yil}\\{ay}\\{gün}\\{subs}.jpg";
                             string klasorAd = $"{tc}\\{yil}\\{ay}\\{gün}";
-                          
+                            string getTarih = $"{yil}-{ay}-{gün}";
+
                             try
                             {
+                                Console.Write(Environment.NewLine + $"{subs} _{PacsBoyutu.Length} : ");
+                                File.Move(orjFile, copyFile);
                                 using (WEINCDENTALEntities db = new WEINCDENTALEntities())
                                 {
                                     adm_pacs pacsYolla = new adm_pacs()
                                     {
+                                        //t_modality = IO,
                                         t_tc = tc,
                                         t_pacspath = copyFile,
                                         t_ip = 0,
-                                        t_createdate = nnow,
+                                        t_createdate = Convert.ToDateTime(getTarih),
                                         t_createuser = "W3",
                                         t_aktif = 1,
                                         t_klasorad = klasorAd,
@@ -83,7 +94,6 @@ namespace WEINCDENTAL_PACSConsole
                                     db.adm_pacs.Add(pacsYolla);
                                     db.SaveChanges();
                                 }
-                                File.Move(orjFile, copyFile);
                                 File.Delete($"D:\\project\\PacsMemory\\{subs}.jpg");
                                 File.Delete($"D:\\project\\PacsMemory\\{subs}.txt");
                                 Console.Write("[AKTARILDI]");
@@ -91,10 +101,76 @@ namespace WEINCDENTAL_PACSConsole
                             catch (Exception ex)
                             {
                                 Console.WriteLine($"HATA OLUŞTU _ DataBaseye Kaydedilemedi.");
+                                Console.WriteLine($"{ex.Message}");
                                 Thread.Sleep(5000);
                                 goto PacsTimer;
                             }
                         }
+                        #endregion
+
+                        #region Morita Panoramik PACS
+                        else
+                        {
+                            string tc = null;
+                            string yil = null;
+                            string ay = null;
+                            string gün = null;
+                            try
+                            {
+                                System.IO.FileInfo PacsBoyutu = new System.IO.FileInfo(item);
+                                //Dosya ADI = 12312312300-2019-07-16
+                                string[] morita = System.Text.RegularExpressions.Regex.Split(subs, "-");
+                                tc = morita[0];
+                                yil = morita[1];
+                                ay = morita[2];
+                                gün = morita[3];
+                                Directory.CreateDirectory("D:project/Pacs/" + tc + "/" + yil + "/" + ay + "/" + gün);
+                                string orjFile = $"D:\\project\\PacsMemory\\{subs}.jpg";
+                                string copyFile = $"D:\\project\\Pacs\\{tc}\\{yil}\\{ay}\\{gün}\\{subs}.jpg";
+                                string klasorAd = $"{tc}\\{yil}\\{ay}\\{gün}";
+                                string getTarih = $"{yil}-{ay}-{gün}";
+
+                                try
+                                {
+                                    Console.Write(Environment.NewLine + $"{subs} _{PacsBoyutu.Length} : ");
+                                    File.Move(orjFile, copyFile);
+                                    using (WEINCDENTALEntities db = new WEINCDENTALEntities())
+                                    {
+                                        adm_pacs pacsYolla = new adm_pacs()
+                                        {
+                                            //t_modality = PX,
+                                            t_tc = tc,
+                                            t_pacspath = copyFile,
+                                            t_ip = 0,
+                                            t_createdate = Convert.ToDateTime(getTarih),
+                                            t_createuser = "W3",
+                                            t_aktif = 1,
+                                            t_klasorad = klasorAd,
+                                            t_resimad = $"{subs}.jpg"
+                                        };
+                                        db.adm_pacs.Add(pacsYolla);
+                                        db.SaveChanges();
+                                    }
+                                    File.Delete($"D:\\project\\PacsMemory\\{subs}.jpg");
+                                    Console.Write("[AKTARILDI]");
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine($"HATA OLUŞTU _ DataBaseye Kaydedilemedi.");
+                                    Console.WriteLine($"{ex.Message}");
+                                    Thread.Sleep(5000);
+                                    goto PacsTimer;
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                //Console.Write(Environment.NewLine);
+                                //Thread.Sleep(5000);
+                                //goto PacsTimer;
+                            }
+
+                        }
+                        #endregion
                     }
                     Console.Write(Environment.NewLine);
                     Thread.Sleep(5000);
@@ -115,10 +191,8 @@ namespace WEINCDENTAL_PACSConsole
 
         }
 
-        static void Main(string[] args)
+        static void KlasorControl()
         {
-            Console.Title = "WEINCREATIVE PACS";
-
         PacsBasla:
             if (System.IO.Directory.Exists("D:project/PacsMemory") && System.IO.Directory.Exists("D:project/Pacs"))
             {
@@ -130,13 +204,13 @@ namespace WEINCDENTAL_PACSConsole
                 Directory.CreateDirectory("D:project/Pacs");
                 goto PacsBasla;
             }
+        }
 
-
-
-
-
-
-
+        #endregion
+        static void Main(string[] args)
+        {
+            Console.Title = "WEINCREATIVE PACS";
+            KlasorControl();
         }
     }
 }
